45a69cb295faebc1fe0247cc54bcd6e2806daabd
Alec Maly <alecjmaly@gmail.com>
2022-11-19 00:33:41 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ require('dotenv').config()
 
 // express config
 const app = express()
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.static(__dirname + '/public'));
 
